85b37491e5da19e53c4e99fc5a666c7f0a90df04
Horacio Borba <gastonborba@gmail.com>
2026-04-01 23:35:36 -0300

    fix 403 error

--- src/main/java/io/pathops/controlplane/config/JWTSecurityConfig.java
+++ src/main/java/io/pathops/controlplane/config/JWTSecurityConfig.java
@@ -2,6 +2,7 @@ package io.pathops.controlplane.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class JWTSecurityConfig {
-	
+
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 	    http.csrf(csrf -> csrf.disable())
@@ -20,7 +21,8 @@ public class JWTSecurityConfig {
 		    .requestMatchers("/actuator/info").permitAll()
 		    .requestMatchers("/actuator/**").denyAll()
 	    	.requestMatchers("/public/login").authenticated()
-	    	.anyRequest().authenticated());
+	    	.anyRequest().authenticated())
+	    .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 	    return http.build();
 	}
 }
